0d4e9fd0d27efb5a8a5cc3850484e2f93aca6939
ErykoItaliano <eryk5683@gmail.com>
2024-06-09 17:19:49 +0200

    Revert "Fix img_url can be null now"
    This reverts commit 31bdf95e4d5bf6ba03adcc8287ce2752c979ec41.

--- Lego_collector/app/src/main/java/com/example/lego_collector/LegoSet.kt
+++ Lego_collector/app/src/main/java/com/example/lego_collector/LegoSet.kt
@@ -20,7 +20,7 @@ data class LegoSet(
     val year: Int,
     val theme_id: Int,
     val num_parts: Int,
-    val set_img_url: String?,
+    val set_img_url: String,
     val set_url: String,
     val last_modified_dt: String
 ) : java.io.Serializable
